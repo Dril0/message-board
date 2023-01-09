dceab5b45b3dd32031cf89ae96d54a5d8ad9ab78
Jeremias <jeremias.acurso@hotmail.com>
2023-01-09 13:51:00 -0300

    Test para comprobar la existencia de la URL, de su nombre como la etiqetea de la url, correcto nombre de la template y el contenido que coincida con la base de datos

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .views import Posts
+from django.urls import reverse
 
 # Create your tests here.
+
+"""cuando testeamos bases de datos se utiliza TestCase, y solo se va a testear las funciones que empiecen con test*"""
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Posts.objects.create(text="This is a Test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a Test!")
+
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "This is a Test!")
